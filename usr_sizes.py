f23f59faf9020f3c9e6e8695c5d22b1784b9ad15
Scott Gillespie <scott.gillespie@netiq.com>
2014-01-21 10:22:09 -0600

    updates

---
+++ usr_sizes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+agent_version='vsau73'
+
+build_systems=[]
+def build_systems_listing():
+    with open ( '/Unix3/Unix/agent/'+agent_version+'/build_tools/SYSTEMS' ) as inf:
+        for line in inf:
+            if not line.strip().startswith("#"):
+                cols=line.split(';')        # SYSTEMS file is semicolon delimited
+                #if cols[0] != "isuse1":     
+                build_systems.append( cols[0] ) 
+
+import subprocess 
+def get_usr_size():
+    with open( 'usr_sizes.txt', 'w' ) as outfile:
+        for host in build_systems:
+            #print ":::"+host+":::"
+            #os.system( '/usr/bin/ssh '+host+' df -k /usr' )
+            subprocess.call(['/usr/bin/ssh', host, 'hostname'], stdout=outfile)
+            subprocess.call(['/usr/bin/ssh', host, 'df -k /usr'], stdout=outfile)
+
+if __name__=='__main__':
+    build_systems_listing()
+    get_usr_size()
