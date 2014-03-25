d6f1fcc2f43a2bca3d7e9faabbedf5d5001a5706
Scott Gillespie <scott.gillespie@netiq.com>
2014-03-25 07:11:22 -0500

    cwdr.pl - listing of cwd contents, largest files first, in MB

--- cwdr.pl
+++ cwdr.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/local/bin/perl
 
 use strict;
 use warnings;
@@ -8,8 +8,12 @@ my @sizes = map -s, @files;
 my %files_sizes = ();
 @files_sizes{@files} = @sizes;
 for my $key ( reverse sort { $files_sizes{$a} <=> $files_sizes{$b} } keys %files_sizes ) {
-    # I use desktop-sized xterms
-    printf "%-50s %20s\n", $key, $files_sizes{$key};
+    printf "%-55s %.2f MB\n", $key, size_in_mb( $files_sizes{$key} );
+}
+
+sub size_in_mb {
+    my $size_in_bytes = shift;
+    return $size_in_bytes / ( 1024 * 1024 ); 
 }
 
 
