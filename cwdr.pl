62ea0f481c6cc7801949003a10fd952b6016867a
Scott Gillespie <scott.gillespie@netiq.com>
2014-03-25 06:50:00 -0500

    adding sample output from cwdr.pl

--- cwdr.pl
+++ cwdr.pl
@@ -8,7 +8,8 @@ my @sizes = map -s, @files;
 my %files_sizes = ();
 @files_sizes{@files} = @sizes;
 for my $key ( reverse sort { $files_sizes{$a} <=> $files_sizes{$b} } keys %files_sizes ) {
-    printf "%-40s %20s\n", $key, $files_sizes{$key};
+    # I use desktop-sized xterms
+    printf "%-50s %20s\n", $key, $files_sizes{$key};
 }
 
 
