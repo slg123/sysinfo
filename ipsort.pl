95a46d6554beeb7bb5442322eec1e4c8c904f33d
Scott Gillespie <scott.gillespie@netiq.com>
2014-04-16 10:15:42 -0500

    update ipsort.pl to sort list of foreign IP addresses

--- ipsort.pl
+++ ipsort.pl
@@ -2,18 +2,33 @@
 
 use strict;
 use warnings;
+use autodie;
 
 use Socket 'inet_aton';
 use List::MoreUtils 'apply';
 
-my $file_name = "visiting_ips.txt";
+my $cmd = "/bin/netstat -na";
+my $visitors = "visiting_ips.txt";
+open my $ofh, ">", $visitors;
+open my $fh, "-|", $cmd;
+while ( <$fh> ) {
+    if ( /ESTABLISHED/ ) {
+        my ( $proto, $recv_q, $send_q, $local_addr, $foreign_addr, $state ) = split ' ';
+        my ( $addr, $port ) = split ':', $foreign_addr;
+        print $ofh "$addr\n";
+    }
+}
+close $fh;
+close $ofh;
 
-open my $fh, "<", $file_name or die "can't open $!";
+open my $ifh, "<", $visitors or die "can't open $!";
 my @sorted = map { substr($_, 4) }
              sort
              map { inet_aton($_) . $_ }
              apply { chomp }
-             <$fh>;
+             <$ifh>;
 
-print "$_\n" foreach @sorted;
-close $fh;
+for my $ip ( @sorted ) {
+    print $ip, "\n";
+}
+close $ifh;
