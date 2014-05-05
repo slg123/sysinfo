f28c2e68661b473272f1ca2641c1587901bf481a
Scott Gillespie <scott.gillespie@netiq.com>
2014-05-05 12:02:31 -0500

    update java process gc doodad

--- java_procs.pl
+++ java_procs.pl
@@ -14,6 +14,9 @@ sub get_pids {
     return @pids;
 }
 
+our ( $total_szero, $total_sone, $total_sou, $total_ec, $total_eu, $total_oc, $total_ou, 
+      $total_pc, $total_pu, $total_ygc, $total_ygct, $total_fgc, $total_fgct, $total_gct ) = 0; 
+
 sub get_jstats {
     my @java_pids = @_;
     foreach my $pid ( @java_pids ) {
@@ -24,6 +27,20 @@ sub get_jstats {
             my ( $szero, $sone, $sou, $ec, $eu, $oc, $ou, $pc, $pu, $ygc, $ygct, $fgc, $fgct, $gct ) = split /\s+/;
             if ( /\d+.\d/ ) {
                 print "$pid: $szero $sone $sou $ec $eu $oc $ou $pc $pu $ygc $ygct $fgc $fgct $gct\n";
+                $total_szero += $szero;
+                $total_sone  += $sone; 
+                $total_sou   += $sou; 
+                $total_ec    += $ec; 
+                $total_eu    += $eu; 
+                $total_oc    += $oc; 
+                $total_ou    += $ou; 
+                $total_pc    += $pc; 
+                $total_pu    += $pu; 
+                $total_ygc   += $ygc; 
+                $total_ygct  += $ygct; 
+                $total_fgc   += $fgc; 
+                $total_fgct  += $fgct; 
+                $total_gct   += $gct;
             }
         }
     }
@@ -32,6 +49,9 @@ sub get_jstats {
 sub main {
     get_pids(); 
     get_jstats( @pids );
+    printf "%.1f %.1f %.1f %.1f %.1f %.1f %.1f %.1f %.1f %.1f %.1f %.1f %.1f %.1f\n", 
+            $total_szero, $total_sone, $total_sou, $total_ec, $total_eu, $total_oc, $total_ou, 
+            $total_pc, $total_pu, $total_ygc, $total_ygct, $total_fgc, $total_fgct, $total_gct; 
 }
 
 main(); 
