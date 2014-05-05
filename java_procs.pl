49475796fb3e2ba4337e242ceef720f9ff7ee0af
Scott Gillespie <scott.gillespie@netiq.com>
2014-05-05 08:02:06 -0500

    update java process gc doodad

--- java_procs.pl
+++ java_procs.pl
@@ -15,25 +15,21 @@ sub get_pids {
 }
 
 sub get_jstats {
-    my $pid = shift; 
-    my $java_garbage_collection_cmd = "/usr/bin/jstat -gc $pid"; 
-    open my $gc_fh, "-|", $java_garbage_collection_cmd;
-    while ( <$gc_fh> ) {
-        if ( /\d+/ ) {
-            my ( $szero, $sone, $sou, $ec, $eu, $oc, $ou, $pc, $pu, $ygc, $ygct, $fgc, $fgct, $gct ) = split /\s+/;
-            print "$szero $sone $pc $pu $gct\n"; 
+    for my $pid ( @pids ) {
+        my $java_garbage_collection_cmd = "/usr/bin/jstat -gc $pid"; 
+        open my $gc_fh, "-|", $java_garbage_collection_cmd;
+        while ( <$gc_fh> ) {
+            if ( /\d+/ ) {
+                my ( $szero, $sone, $sou, $ec, $eu, $oc, $ou, $pc, $pu, $ygc, $ygct, $fgc, $fgct, $gct ) = split /\s+/;
+                print "$szero $sone $pc $pu $gct\n"; 
+            }
         }
     }
 }
 
 sub main {
     get_pids(); 
-    for ( @pids ) {
-        print "DEBUG: $_";
-    }
-    for ( @pids ) {
-        get_jstats( $_ );
-    }
+    get_jstats();
 }
 
 main(); 
