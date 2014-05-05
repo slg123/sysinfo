89c59e9c984549aeaa1f5cc11fe5e277e2efd7d1
Scott Gillespie <scott.gillespie@netiq.com>
2014-05-05 07:32:08 -0500

    update java process gc doodad

--- java_procs.pl
+++ java_procs.pl
@@ -4,29 +4,36 @@ use strict;
 use warnings;
 use autodie;
 
-sub get_java_process_info {
-
-    my $java_processes_cmd = "/bin/ps -C java v"; 
-    open my $ps_fh, "-|", $java_processes_cmd;
+our @pids;
+sub get_pids {
+    my $cmd = "ps -ef | grep java | awk '{ print \$2 }'"; 
+    open my $ps_fh, "-|", $cmd;
     while ( <$ps_fh> ) {
-        my ( $pid, $tty, $stat, $time, $majfl, $trs, $drs, $rss, $mem, @command ) = split /\s+/; 
-        my $java_process = join( " ", @command ); 
-        print "$mem $java_process\n"; 
+        push @pids, $_;
     }
-    close $ps_fh;
+    return @pids;
+}
 
-    my @pids = `ps -C java | awk '{ print \$1 }' | grep -v PID`; 
-
-    foreach my $pid ( @pids ) {
-        my $java_garbage_collection_cmd = "/usr/bin/jstat -gc $pid"; 
-        open my $gc_fh, "-|", $java_garbage_collection_cmd;
-        while ( <$gc_fh> ) {
-            if ( /\d+/ ) {
-                my ( $szero, $sone, $sou, $ec, $eu, $oc, $ou, $pc, $pu, $ygc, $ygct, $fgc, $fgct, $gct ) = split /\s+/;
-                print "$szero $sone $pc $pu $gct\n"; 
-            }
+sub get_jstats {
+    my $pid = shift; 
+    my $java_garbage_collection_cmd = "/usr/bin/jstat -gc $pid"; 
+    open my $gc_fh, "-|", $java_garbage_collection_cmd;
+    while ( <$gc_fh> ) {
+        if ( /\d+/ ) {
+            my ( $szero, $sone, $sou, $ec, $eu, $oc, $ou, $pc, $pu, $ygc, $ygct, $fgc, $fgct, $gct ) = split /\s+/;
+            print "$szero $sone $pc $pu $gct\n"; 
         }
     }
 }
 
-get_java_process_info(); 
+sub main {
+    get_pids(); 
+    for ( @pids ) {
+        print "DEBUG: $_";
+    }
+    for ( @pids ) {
+        get_jstats( $_ );
+    }
+}
+
+main(); 
