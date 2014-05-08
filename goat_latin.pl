c8367be5645d17a71a53872e3f554b8ea1ebe6a5
Scott Gillespie <scott.gillespie@netiq.com>
2014-05-08 14:32:01 -0500

    goat latin example lulz

--- goat_latin.pl
+++ goat_latin.pl
@@ -4,21 +4,27 @@ use strict;
 use warnings;
 
 my $string = 'I speak a bit of Goat Latin';
-my @array = split / /, $string;
-my @new_array = {}; 
 
-my $counter = 0;
-for ( @array ) {
-    if ( $_ =~ /^[aeiouy]/i ) {
-        my $goat_latin_vowel_string = $_.add_some_as( $counter );
-        push @new_array, $goat_latin_vowel_string;
-    } else {
-        my $first_char = substr( $_, 0, 1 ); 
-        substr( $_, 0, 1, ""); 
-        my $goat_latin_consonant_string = $_.$first_char.add_some_as( $counter ); 
-        push @new_array, $goat_latin_consonant_string;
+sub convert_to_goat_latin {
+    my $str = shift;
+    my @array = split / /, $str;
+    my @new_array = {}; 
+
+    my $counter = 0;
+    for ( @array ) {
+        if ( $_ =~ /^[aeiouy]/i ) {
+            my $goat_latin_vowel_string = $_.add_some_as( $counter );
+            push @new_array, $goat_latin_vowel_string;
+        } else {
+            my $first_char = substr( $_, 0, 1 ); 
+            substr( $_, 0, 1, ""); 
+            my $goat_latin_consonant_string = $_.$first_char.add_some_as( $counter ); 
+            push @new_array, $goat_latin_consonant_string;
+        }
+        $counter++;
     }
-    $counter++;
+    my $goat_latin_string = join( " ", @new_array ); 
+    return $goat_latin_string;
 }
 
 sub add_some_as {
@@ -26,8 +32,8 @@ sub add_some_as {
     return "a" x $quantity;
 }
 
-my $goat_latin_string = join( " ", @new_array );
-print $goat_latin_string, "\n"; 
+my $newstring = convert_to_goat_latin( $string ); 
+print $newstring, "\n"; 
 
 
 
