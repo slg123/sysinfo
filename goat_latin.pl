e7383de91a6656499f4dedccac41e1715ffea345
Scott Gillespie <scott.gillespie@netiq.com>
2014-05-08 14:10:27 -0500

    goat latin example lulz

--- goat_latin.pl
+++ goat_latin.pl
@@ -14,6 +14,7 @@ for ( @array ) {
         push @new_array, $goat_latin_vowel_string;
     } else {
         my $first_char = substr( $_, 0, 1 ); 
+        substr( $_, 0, 1, ""); 
         my $goat_latin_consonant_string = $_.$first_char.add_some_as( $counter ); 
         push @new_array, $goat_latin_consonant_string;
     }
