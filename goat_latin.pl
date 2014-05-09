303f1951daab05f732cde1778c7543796c1711cd
Scott Gillespie <scott.gillespie@netiq.com>
2014-05-09 11:13:31 -0500

    goat latin example lulz

--- goat_latin.pl
+++ goat_latin.pl
@@ -8,23 +8,23 @@ my $string = 'I speak a bit of Goat Latin';
 sub convert_to_goat_latin {
     my $str = shift;
     my @array = split / /, $str;
-    my @new_array = {}; 
+    my @new_array;
 
     my $counter = 0;
     for ( @array ) {
         if ( $_ =~ /^[aeiouy]/i ) {
-            my $goat_latin_vowel_string = $_.add_some_as( $counter );
-            push @new_array, $goat_latin_vowel_string;
+            my $vowel_string = $_.add_some_as( $counter );
+            push @new_array, $vowel_string;
         } else {
             my $first_char = substr( $_, 0, 1 ); 
             substr( $_, 0, 1, ""); 
-            my $goat_latin_consonant_string = $_.$first_char.add_some_as( $counter ); 
-            push @new_array, $goat_latin_consonant_string;
+            my $consonant_string = $_.$first_char.add_some_as( $counter ); 
+            push @new_array, $consonant_string; 
         }
         $counter++;
     }
-    my $goat_latin_string = join( " ", @new_array ); 
-    return $goat_latin_string;
+    my $gl_string = join( " ", @new_array ); 
+    return $gl_string;
 }
 
 sub add_some_as {
