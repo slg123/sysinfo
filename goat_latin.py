0fff58def3744a310e26ecf509293639c0830c79
Scott Gillespie <scott.gillespie@netiq.com>
2014-05-13 13:00:12 -0500

    goat latin python example

---
+++ goat_latin.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+def convert_to_goat_latin( str ):
+    out = []
+    words = string.split()
+    counter = 0
+    for word in words:
+        if word[0] == "aeiouy":
+            out.append( ( word[1:] ) + ( "a" * counter ) )
+        else:
+            first_char = word[0:1]
+            out.append(( word[1:] ) + first_char + ( "a" * counter ) )
+        counter += 1
+    return out
+
+string = 'I speak a bit of Goat Latin'
+print ' '.join( convert_to_goat_latin( string ) )
