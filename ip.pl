c24b1b3f1544a12312d5cd293b86cd55b60a59d2
Scott Gillespie <scott.gillespie@netiq.com>
2014-05-15 13:02:49 -0500

    updating ip.pl

--- ip.pl
+++ ip.pl
@@ -3,34 +3,47 @@
 use strict;
 use warnings;
 
-# get ips and hostnames on vlan 522 and vlan 174
-
 my @vlans = qw( 10.22.23/23 10.21.74/24 );
+my %ips_hosts = ();
 
-for ( @vlans ) {
-    if ( /10\.22\.23/  ) {
-        for ( my $i = 22; $i < 24; $i++ ) {
-            for ( my $j = 2; $j < 256; $j++ ) {
-                my $system = `/usr/bin/host 10.22.$i.$j`;
-                if ( $system !~ /NXDOMAIN/ ) {
-                    my @foo = split( " ", $system );
-                    my $hostname = $foo[4];
-                    print "10.22.$i.$j    $hostname\n";
+sub build_ip_hosts_list {
+    for ( @vlans ) {
+        if ( /10\.22\.23/  ) {
+            for ( my $i = 22; $i < 24; $i++ ) {
+                for ( my $j = 2; $j < 256; $j++ ) {
+                    my $system = `/usr/bin/host 10.22.$i.$j`;
+                    if ( $system !~ /NXDOMAIN/ ) {
+                        my @foo = split( " ", $system );
+                        my $hostname = $foo[4];
+                        my $ip = "10.22.$i.$j";
+                        $ips_hosts{ $ip } = $hostname; 
+                    }
                 }
             }
         }
-    }
 
-    if ( /10\.21\.74/ ) {
-        for ( my $i = 2; $i < 256; $i++ ) {
-            my $system = `/usr/bin/host 10.21.74.$i`;
-            if ( $system !~ /NXDOMAIN/ ) {
-                my @foo = split( " ", $system );
-                my $hostname = $foo[4];
-                print "10.21.74.$i    $hostname\n"; 
+        if ( /10\.21\.74/ ) {
+            for ( my $i = 2; $i < 256; $i++ ) {
+                my $system = `/usr/bin/host 10.21.74.$i`;
+                if ( $system !~ /NXDOMAIN/ ) {
+                    my @foo = split( " ", $system );
+                    my $hostname = $foo[4];
+                    my $ip = "10.21.74.$i";
+                    $ips_hosts{ $ip } = $hostname; 
+                }
             }
         }
     }
 }
 
+sub print_ip_hosts {
+    while ( my ( $key, $value ) = each %ips_hosts ) {
+        print "$key    $value\n"; 
+    }
+}
 
+sub main {
+    build_ip_hosts_list();
+    print_ip_hosts();
+}
+main()
