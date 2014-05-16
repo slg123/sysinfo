2bb242dbbebe06c737e4e664c2ca9879dcd99805
Scott Gillespie <scott.gillespie@netiq.com>
2014-05-16 11:23:08 -0500

    adding aix memory percent used calculator

--- aixmem.pl
+++ aixmem.pl
@@ -10,12 +10,10 @@ sub get_aix_mem_percent_used {
         if ( /memory/ ) {
             my @line = split /\s{2,}/;
             $memory = $line[1];
-            print "DEBUG: $memory\n"; 
         }
         if ( /in use/ ) {
             my @line = split /\s{2,}/;
             $in_use = $line[1];
-            print "DEBUG: $in_use\n"; 
         }
         if ( $memory != 0 ) {
             $percent_used = $in_use / $memory * 100;
