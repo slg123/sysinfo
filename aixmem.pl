e2673946fbe053b9b93e3f783c598123d5b23b64
Scott Gillespie <scott.gillespie@netiq.com>
2014-05-16 13:47:52 -0500

    update

--- aixmem.pl
+++ aixmem.pl
@@ -4,11 +4,14 @@ use strict;
 use warnings;
 use autodie;
 
+#
+# Get AIX memory as percentage of system RAM in use.
+#
 sub get_aix_mem_percent_used {
 
     my $percent_used = 0;
-    my $in_use = 0;
-    my $memory = 0;
+    my $in_use       = 0;
+    my $memory       = 0;
 
     my $cmd = "svmon -G";
     open my $fh, "-|", $cmd;
@@ -30,5 +33,23 @@ sub get_aix_mem_percent_used {
     return $percent_used;
 }
 
-my $pct_inuse = get_aix_mem_percent_used();
-printf "%0.2f\n", $pct_inuse;
+sub get_aix_paging_percent_used {
+
+    my $percent_used = 0;
+    my $cmd = "lsps -s";
+    open my $fh, "-|", $cmd;
+    while ( <$fh> ) {
+        if ( /MB/ ) {
+            my ( $foo, $size, $percent ) = split /\s+/;
+            $percent_used = substr( $percent, 0, -1 );
+        }
+    }
+    return $percent_used;
+}
+
+sub main() {
+    my $pct_mem_inuse = get_aix_mem_percent_used();
+    my $pct_paging    = get_aix_paging_percent_used();
+    printf "memory: %0.2f percent \npaging: %0.2f percent\n", $pct_mem_inuse, $pct_paging;
+}
+main();
