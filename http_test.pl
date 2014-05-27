1af6b80fb6c92a8f32c10477d946d1a77e16fada
Scott Gillespie <scott.gillespie@netiq.com>
2014-05-27 10:18:24 -0500

    http test perl

---
+++ http_test.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use IO::Socket;
+
+my $HttpPort = 8888;
+our $port;
+our $url = "/index.html";
+our $address = 'om.houqe.lab';
+$port = $HttpPort;
+print "DEBUG: $port\n";
+
+my $connection;
+
+if ( $port ) {
+    print "connecting to $address:$port\n"; 
+    $connection = IO::Socket::INET->new(Proto => "tcp", 
+        PeerAddr => "$address:$port",
+        Timeout  => 15 
+    );
+} else {
+    $connection = IO::Socket::INET->new(
+        Proto    => "tcp",
+        PeerAddr => "http($port)",
+        Timeout  => 15
+    );
+}
+
+$connection->autoflush(1);
+print "DEBUG: $connection ->" . "HEAD $url HTTP/1.0" . "\015\012" . "\015\012";
+print "DEBUG: $connection\n"; 
+my $line = <$connection>;
+
