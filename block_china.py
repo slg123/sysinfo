faf4708d964e23c9cb9ed786fb25df1b5bb85d07
Scott Gillespie <scott.gillespie@netiq.com>
2014-06-17 07:46:53 -0500

    updated china cidr block script

--- block_china.py
+++ block_china.py
@@ -3,21 +3,23 @@
 import wget
 import os
 
-def download_file():
-    filename = 'chinacidr.txt'
-    os.remove( filename ) if os.path.exists( filename ) else None
+filename = 'chinacidr.txt'
 
+def download_file():
+    os.remove( filename ) if os.path.exists( filename ) else None
     url = 'http://www.okean.com/chinacidr.txt'
     wget.download( url )
 
 def create_blocking_tables():
     f = open( filename, 'r' )
     for line in f:
-        cidr_ip = line.split(' ')[0]
-        os.system( "iptables -I INPUT -s cidr_ip -j DROP" )
+        if not line.startswith( "#" ):    # skip commented lines
+            cidr_ip = line.split(' ')[0]
+            os.system( "iptables -I INPUT -s "+cidr_ip+" -j DROP" )
 
 def main():
     download_file()
+    print "\n[+] creating blocking tables... this will take some time."
     create_blocking_tables()
 
 if __name__ == "__main__":
