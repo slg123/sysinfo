bb6ede1e57c9b44adf27b41454ec33d153502475
Scott Gillespie <scott.gillespie@netiq.com>
2014-06-25 13:19:52 -0500

    convert to minutes if s > 60

--- fib_no_memoization.pl
+++ fib_no_memoization.pl
@@ -9,10 +9,20 @@ sub fib {
     return fib( $n-1 ) + fib( $n-2 ); 
 }
 
+sub to_minutes {
+    my $seconds = shift;
+    return $seconds if $seconds < 60;
+    return ( $seconds / 60 ); 
+
 for ( my $i = 0; $i < 100; $i++ ) {
     my $start = time();
     my $val = fib( $i ); 
     my $finish = time(); 
     my $seconds = $finish - $start;
-    print "$seconds seconds to calculate fib($i): $val\n"; 
+    if ( $seconds < 60 ) {
+        print "$seconds seconds to calculate fib($i): $val\n"; 
+    } else {
+        my $minutes = to_minutes( $seconds );
+        print "$minutes minutes to calculate fib($i): $val\n"; 
+    }
 }
